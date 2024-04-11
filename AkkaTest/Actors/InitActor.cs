b29dfad7317f97a8b9ae1b4740ecc3ac3e46c542
Aaron Stannard <aaron@petabridge.com>
2024-04-10 22:50:21 -0500

    added additional logging

--- AkkaTest/Actors/InitActor.cs
+++ AkkaTest/Actors/InitActor.cs
@@ -1,10 +1,13 @@
 using Akka.Actor;
+using Akka.Event;
 using AkkaTest.Messages;
 
 namespace AkkaTest.Actors;
 
 public class InitActor : ReceiveActor
 {
+    private readonly ILoggingAdapter _logger = Context.GetLogger();
+    
     public InitActor()
     {
         ReceiveAsync<InitRequestMessage>(Handler);
@@ -12,13 +15,19 @@ public class InitActor : ReceiveActor
 
     private async Task Handler(InitRequestMessage initMsg)
     {
-        // Do something
+        _logger.Info("First step - Sender: {0}", Sender);
+        
+        // Do something'
         await Task.Delay(1);
         
         Sender.Tell(new InitResponseMessage(), Self);
         
+        _logger.Info("Second step - Sender: {0}", Sender);
+        
         //await Task.Delay(1); // Uncomment this line to fix the test, with 1.5.15 works without uncomment
         
         Sender.Tell(new StartRequestMessage(), Self);
+        
+        _logger.Info("Third step - Sender: {0}", Sender);
     }
 }
